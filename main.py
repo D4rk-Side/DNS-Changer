3686250ada4cb3d99e21163d73aacdd4e5b7c164
D4rk $ide <d4rkside@duck.com>
2026-02-23 13:18:33 -0500

    Revert "New updates."

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
-import subprocess, re, os, time, platform
+import subprocess, re, os, time
 
 dns_dict = {
-    'AdGuard (Family)': ['176.103.130.130', '176.103.130.131'],
-    'AdGuard (Gaming)': ['94.140.14.14', '94.140.15.15'],
-    'Alternate': ['76.76.19.19', '76.223.122.150'],
+    'Cloudflare' : ['1.1.1.1', '1.0.0.1'],
+    'Google public' : ['8.8.8.8', '8.8.4.4'],
+    'OpenDNS' : ['208.67.222.222', '208.67.220.220'],
+    'Shecan' : ['178.22.122.100', '185.51.200.2'],
     'Azad(403)' : ['10.202.10.202', '10.202.10.102'],
     'Begzar' : ['185.55.226.26', '185.55.225.25'],
-    'Beshkan': ['181.41.194.177', '181.41.194.186'],
-    'CenturyLink': ['205.171.3.65', '205.171.2.65'],
-    'CleanBrowsing' : ['185.228.168.9', '185.228.169.9'],
-    'Cloudflare' : ['1.1.1.1', '1.0.0.1'],
-    'Comodo Secure' : ['8.26.56.26', '8.20.247.20'],
-    'ControlD': ['76.76.2.0', '76.76.10.0'],
-    'DNS.Watch': ['84.200.69.80', '84.200.70.40'],
-    'DYN': ['216.146.35.35', '216.146.36.36'],
     'Electro' : ['78.157.42.100', '78.157.42.101'],
-    'FreeDNS': ['37.235.1.174', '37.235.1.177'],
-    'Google public' : ['8.8.8.8', '8.8.4.4'],
-    'Level3': ['209.244.0.3', '209.244.0.4'],
-    'Neustar': ['156.154.70.22', '156.154.71.22'],
-    'NextDNS': ['45.90.28.0', '45.90.30.0'],
-    'OpenDNS' : ['208.67.222.222', '208.67.220.220'],
-    'OpenNIC': ['216.87.84.211', '23.90.4.6'],
-    'Pishgaman': ['5.202.100.100', '5.202.100.101'],
     'Quad9' : ['9.9.9.9', '149.112.112.112'],
-    'Radar': ['10.202.10.10', '10.202.10.11'],
-    'Safe': ['195.46.39.39', '195.46.39.40'],
-    'Shecan' : ['178.22.122.100', '185.51.200.2'],
-    'Shelter public': ['94.103.125.157', '94.103.125.158'],
-    'SmartViper': ['208.76.50.50', '208.76.51.51'],
+    'CleanBrowsing' : ['185.228.168.9', '185.228.169.9'],
+    'Comodo Secure' : ['8.26.56.26', '8.20.247.20'],
+    'Alternate': ['76.76.19.19', '76.223.122.150'],
+    'OpenNIC': ['216.87.84.211', '23.90.4.6'],
     'Verisign public': ['64.6.64.6', '64.6.65.6'],
+    'Level3': ['209.244.0.3', '209.244.0.4'],
+    'DYN': ['216.146.35.35', '216.146.36.36'],
+    'Safe': ['195.46.39.39', '195.46.39.40'],
+    'DNS.Watch': ['84.200.69.80', '84.200.70.40'],
+    'Shelter public': ['94.103.125.157', '94.103.125.158'],
+    'Beshkan': ['181.41.194.177', '181.41.194.186'],
+    'Radar': ['10.202.10.10', '10.202.10.11'],
+    'Pishgaman': ['5.202.100.100', '5.202.100.101'],
+    'NextDNS': ['45.90.28.0', '45.90.30.0'],
+    'ControlD': ['76.76.2.0', '76.76.10.0'],
+    'AdGuard (Family)': ['176.103.130.130', '176.103.130.131'],
+    'AdGuard (Gaming)': ['94.140.14.14', '94.140.15.15'],
+    'SmartViper': ['208.76.50.50', '208.76.51.51'],
+    'CenturyLink': ['205.171.3.65', '205.171.2.65'],
+    'Neustar': ['156.154.70.22', '156.154.71.22'],
+    'FreeDNS': ['37.235.1.174', '37.235.1.177'],
     'Yandex (Basic)': ['77.88.8.8', '77.88.8.1'],
-    'Yandex (Family)': ['77.88.8.7', '77.88.8.3'],
-    'Yandex (Safe)': ['77.88.8.88', '77.88.8.2']
+    'Yandex (Safe)': ['77.88.8.88', '77.88.8.2'],
+    'Yandex (Family)': ['77.88.8.7', '77.88.8.3']
     }
-system, node, release, version, machine, cpu = platform.uname()
 print('Welcome to DNS changer')
 dl = 0
 dl_head = ('     ' + 42 * '_')  # 'dl_head' -> Dns list header
@@ -121,36 +120,26 @@ if uo == '1':  # Linux section
         time.sleep(2)
         os.system('clear')
 
-                                for network in cwnf:
-                                    num += 1
-                                    nn = (int(abs(float(len(network) - 48) // 2)) * ' ' + f'{network}' + int(abs(float(len(network) - 48) / 2)) * ' ' )  # 'nn' -> Network name
-                                    nc = (f'        ({num})         ')  # 'nc' -> Network code
-                                    print('    |      Wi-Fi      |' + nn + '|' + nc + '|', body, sep='\n')
-                                    nl.append(network)
-                                for i in range(1, len(nl) + 1):
-                                    valid_networks.append(str(i))
-                                while True:
-                                    ns = input('select your network: ')  # 'ns' -> Network select
-                                    if ns in valid_networks:
-                                        cpds = subprocess.run(['netsh', 'interface', 'ipv4', 'set', 'dnsservers', f'name="{nl[int(ns) - 1]}"', 'source=dhcp'])
-                                        print(f"Now your DNS set on Dynamic Host Configuration Protocol(DHCP) \nGood luck!")
-                                        time.sleep(2)
-                                        os.system('cls')
-                                        break
-                                    else:
-                                        print('Please enter a valid network code.')
-                            case _:
-                                num = 0
-                                nl = []  # 'nl' -> Network list
-                                head = ('    ' + 89 * '_')
-                                body = ('    |' + 87 * '-' + '|')
-                                print(head, '    | Connection type |' + (18 * ' ') + 'Network name' + (18 * ' ') + '|   Network number   |', body, sep='\n')
-                                for network in cenf:
-                                    num += 1
-                                    nn = (int(abs(float(len(network) - 48) // 2)) * ' ' + f'{network}' + int(abs(float(len(network) - 48) / 2)) * ' ' )  # 'nn' -> Network name
-                                    nc = (f'        ({num})         ')  # 'nc' -> Network code
-                                    print('    |     Ethernet    |' + nn + '|' + nc + '|', body, sep='\n')
-                                    nl.append(network)
+elif uo == '2':  # Windows section
+    cnc = subprocess.run(['ipconfig', '/all'], capture_output=True).stdout.decode()  # 'cnc' -> cmd network check
+    cenf = list(re.findall('Ethernet adapter (.*):', cnc))  # 'cenf' -> cmd ethernet network find
+    cwnf = list(re.findall('Wireless LAN adapter (.*):', cnc))  # 'cwnf' -> cmd wifi network find
+    if sd == 'f':
+        cfd = subprocess.run(['ipconfig', '/flushdns'])  # 'cfd' -> Cmd flush DNS
+        time.sleep(2)
+        os.system('cls')
+    else:
+        num = 0
+        nl = []  # 'nl' -> Network list
+        head = ('    ' + 89 * '_')
+        body = ('    |' + 87 * '-' + '|')
+        print(head, '    | Connection type |' + (18 * ' ') + 'Network name' + (18 * ' ') + '|   Network number   |', body, sep='\n')
+        for network in cenf:
+            num += 1
+            nn = (int(abs(float(len(network) - 48) // 2)) * ' ' + f'{network}' + int(abs(float(len(network) - 48) / 2)) * ' ' )  # 'nn' -> Network name
+            nc = (f'        ({num})         ')  # 'nc' -> Network code
+            print('    |     Ethernet    |' + nn + '|' + nc + '|', body, sep='\n')
+            nl.append(network)
 
         for network in cwnf:
             num += 1
